f668da14ead9867979e54115fac7c3e5425ee2eb
Jannis <0limitdev@gmail.com>
2026-03-29 22:15:45 +0200

    remove unused repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
-    maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
     maven { url = uri("https://jitpack.io") }
 }
 
